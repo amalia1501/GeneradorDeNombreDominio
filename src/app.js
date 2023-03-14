164f55bf1024676f6217debfd9dbfdbe2a832dd5
amalia1501 <amibottini@hotmail.com>
2023-03-14 01:19:06 +0000

    Ejercicio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,15 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+for (let index = 0; index < pronoun.length; index++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(pronoun[index] + adj[j] + noun[k] + ".com");
+    }
+  }
+}
